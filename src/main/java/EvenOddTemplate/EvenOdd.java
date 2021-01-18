c233420b8a72ce0f6143f3129028ebfb51e92111
dDyman1 <ddyman@student.touro.edu>
2021-01-18 10:32:48 -0500

    Commit

--- src/main/java/EvenOddTemplate/EvenOdd.java
+++ src/main/java/EvenOddTemplate/EvenOdd.java
@@ -5,21 +5,10 @@ public class EvenOdd {
 
     public static void main(String[] args) {
         // TODO: Accept user input using a Scanner
-        Scanner scan = new Scanner(System.in);
-        while (scan.hasNextInt()) {
-            int n = scan.nextInt();
-            boolean even = EvenOdd.isEven(n);
-            if (even) {
-                System.out.println(n + " is an even number.");
-            } else {
-                System.out.println(n + " is an odd number.");
-            }
-        }
     }
 
     public static boolean isEven(int number) {
         // TODO: Determine whether or not the number is even
-        return number%2 == 0;
     }
 
 }
